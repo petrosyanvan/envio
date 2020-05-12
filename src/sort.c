f4186574effec835154bdbb384b9289202f00193
Van Petrosyan <v.petrosyan@1aim.com>
2020-05-12 21:07:15 +0200

    style: convert indentation to spaces

--- src/sort.c
+++ src/sort.c
@@ -10,14 +10,14 @@
  * @Param len - [in] length of the array
  */
 void print_array(int* arr, size_t len) {
-	printf("[ ");
-	for (size_t i = 0; i < len; i++) {
-		printf("%d ", arr[i]);
-	}
-	printf("]\n");
+    printf("[ ");
+    for (size_t i = 0; i < len; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("]\n");
 }
 
-/*
+/*  
  * Function to swap the variables in given two addresses
  *
  * @Param a - [in] address of the first variable
@@ -25,10 +25,10 @@ void print_array(int* arr, size_t len) {
  *
  */
 void swap(int* a, int* b) {
-	int t;
-	t = *b;
-	*b = *a;
-	*a = t;
+    int t;
+    t = *b;
+    *b = *a;
+    *a = t;
 }
 
 /*
@@ -40,76 +40,76 @@ void swap(int* a, int* b) {
  * Return - [out]  0 on success, and -1 if an invalid array was passed 
  */
 int asort(int* arr, size_t len) {
-	size_t left = 0, mid = 0;
-	if (len == 0) {
-		/* empty array was passed*/
-		return -1;
-	}
-	size_t right = len - 1;
+    size_t left = 0, mid = 0;
+    if (len == 0) {
+        /* empty array was passed*/
+        return -1;
+    }
+    size_t right = len - 1;
 
-	// 1 will be our pivot, 0s will be placed on the left side of the pivot and 2s on
-	// the right side of the pivot
-	while (mid <= right) {
-		switch (arr[mid]) {
-			case 0:
-				swap(&arr[left], &arr[mid]);
-				++left;
-				++mid;
-				break;
-			case 1:
-				++mid;
-				break;
-			case 2:
-				swap(&arr[mid], &arr[right]);  
-				--right;     
-				break;
-			default:
-			 	/* not allowed value */
-				return -1;
+    // 1 will be our pivot, 0s will be placed on the left side of the pivot and 2s on
+    // the right side of the pivot
+    while (mid <= right) {
+        switch (arr[mid]) {
+            case 0:
+                swap(&arr[left], &arr[mid]);
+                ++left;
+                ++mid;
+                break;
+            case 1:
+                ++mid;
+                break;
+            case 2:
+                swap(&arr[mid], &arr[right]);  
+                --right;     
+                break;
+            default:
+                /* not allowed value */
+                return -1;
 
-		}
-	}
+        }
+    }
 
-	return 0;
+    return 0;
 }
 
 int main() {
-	/* perform several tests on asort function */
-	int a[] = { 2, 2, 1, 0, 1, 0, 1, 1, 0, 2, 2, 1, 1, 1, 2, 0, 0, 1, 1, 0, 1, 0, 1, 2, 0, 0, 0, 1, 1, 2 };
-	size_t len = sizeof(a) / sizeof(a[0]);
-	if (!asort(a, len)) {
-		print_array(a, len);
-	}
-	else {
-		printf("Invalid array was passed\n");
-	}
+    /* perform several tests on asort function */
+    int a[] = { 2, 2, 1, 0, 1, 0, 1, 1, 0, 2, 2, 1, 1, 1, 2, 0, 0, 1, 1, 0, 1, 0, 1, 2, 0, 0, 0, 1, 1, 2 };
+    size_t len = sizeof(a) / sizeof(a[0]);
+    if (!asort(a, len)) {
+        print_array(a, len);
+    }
+    else {
+        printf("Invalid array was passed\n");
+    }
 
-	int b[] = { 2, 0, 1, 0, 1, 2, 1 };
-	len = sizeof(b) / sizeof(b[0]);
-	if (!asort(b, len)) {
-		print_array(b, len);
-	}
-	else {
-		printf("Invalid array was passed\n");
-	}
+    int b[] = { 2, 0, 1, 0, 1, 2, 1 };
+    len = sizeof(b) / sizeof(b[0]);
+    if (!asort(b, len)) {
+        print_array(b, len);
+    }
+    else {
+        printf("Invalid array was passed\n");
+    }
 
-	int c[] = { 2, 0, 1, 0, 1, 2, -1 };
-	len = sizeof(c) / sizeof(c[0]);
-	if (!asort(c, len)) {
-		print_array(c, len);
-	}
-	else {
-		printf("Invalid array was passed\n");
-	}
+    int c[] = { 2, 0, 1, 0, 1, 2, -1 };
+    len = sizeof(c) / sizeof(c[0]);
+    if (!asort(c, len)) {
+        print_array(c, len);
+    }
+    else {
+        printf("Invalid array was passed\n");
+    }
 
-	int d[] = { };
-	len = sizeof(d) / sizeof(d[0]);
-	if (!asort(d, len)) {
-		print_array(d, len);
-	}
-	else {
-		printf("Invalid array was passed\n");
-	}
+    int d[] = { };
+    len = sizeof(d) / sizeof(d[0]);
+    if (!asort(d, len)) {
+       print_array(d, len);
+    }
+    else {
+       printf("Invalid array was passed\n");
+    }
 
-	return 0;
+    return 0;
 }
